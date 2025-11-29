434bdf4803a07ac7bfe9206a369ac178d1ced876
mgutierrez1477 <mgutierrez1477@gmail.com>
2025-11-28 21:14:27 -0600

    yaaaaaa

--- start.sh
+++ start.sh
@@ -4,6 +4,9 @@
 echo "Starting database migrations..."
 python manage.py migrate
 
+echo "Attempting to create superuser..."
+python manage.py createsuperuser --noinput
+
 # 2. Iniciar el servidor Gunicorn
 echo "Starting Gunicorn server..."
 gunicorn blog_project.wsgi
