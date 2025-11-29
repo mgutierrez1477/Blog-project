7c78cbfc870e997967e50b3aee5bb5e105114cda
mgutierrez1477 <mgutierrez1477@gmail.com>
2025-11-28 20:14:52 -0600

    finalizado

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -102,13 +102,23 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+if DEBUG:
+    # 1. CONFIGURACIÓN LOCAL (modo DEBUG: True) - USA SQLITE
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    # 2. CONFIGURACIÓN DE PRODUCCIÓN (modo DEBUG: False) - USA RENDER/POSTGRESQL
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=config("DATABASE_URL"),
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
